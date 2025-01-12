f4ff2486642cad9c3c3c3393c8c9c73990c62728
geoffolson <geoffrey.olson.jr@gmail.com>
2025-01-12 02:08:08 -0800

    move across grid

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 while True:
-	if can_harvest():
-		harvest()
-	else:
-		move(North)
+	for i in range(get_world_size()):
+		move(East)
+		for j in range(get_world_size()):
+			if can_harvest():
+				harvest()
+			else:
+				plant(Entities.Bush)
+				move(North)
