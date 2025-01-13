49cab26f689ef2e3f9ef9504cf57e4a0951d0261
Geoffrey Olson <geoffrey.olson.jr@gmail.com>
2025-01-12 19:00:41 -0800

    add hay farming function

--- main.py
+++ main.py
@@ -8,28 +8,56 @@ def till_map():
 
 def carrots():
 	till_map()
-	while num_items(Items.Wood) > 1:
+	while num_items(Items.Wood) > 1 and num_items(Items.Hay) > 1:
 		for i in range(get_world_size()):
 			for j in range(get_world_size()):
-				harvest()
-				plant(Entities.Carrot)
+				if can_harvest():
+					harvest()
+					plant(Entities.Carrot)
+				elif get_entity_type() != Entities.Carrot:
+					plant(Entities.Carrot)
 				move(North)
 			move(East)
+
+def can_plant_tree():
+	x_even = get_pos_x() % 2 == 0
+	y_even = get_pos_y() % 2 == 0
+	if x_even and y_even:
+		return True
+	if not x_even and not y_even:
+		return True
+	return False
 
 def wood():
 	clear()
 	while num_items(Items.Wood) < 1000:
 		for i in range(get_world_size()):
-			move(East)
 			for j in range(get_world_size()):
 				if can_harvest():
 					harvest()
-				plant(Entities.Bush)
+				if can_plant_tree():
+					plant(Entities.Tree)
+				else:
+					plant(Entities.Bush)
 				move(North)
+			move(East)
+
+def hay():
+	clear()
+	while num_items(Items.Hay) < 1000:
+		for i in range(get_world_size()):
+			for j in range(get_world_size()):
+				if not can_harvest():
+					do_a_flip()
+				harvest()
+				move(North)
+			move(East)
 
 
 while True:
-	if num_items(Items.Wood) > 1:
+	if num_items(Items.Wood) > 1 and num_items(Items.Hay) > 1:
 		carrots()
+	elif num_items(Items.Hay) < 1:
+		hay()
 	else:
 		wood()
