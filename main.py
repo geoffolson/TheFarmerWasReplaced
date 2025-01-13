a51adb216df31e831047fc725443710dbd855dfe
Geoffrey Olson <geoffrey.olson.jr@gmail.com>
2025-01-12 19:15:21 -0800

    minor refactor

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
-def till_map():
-	clear()
+def for_each(callback):
 	for i in range(get_world_size()):
 		for j in range(get_world_size()):
-			till()
+			callback()
 			move(North)
 		move(East)
 
+
+def till_map():
+	clear()
+	for_each(till)
+
 def carrots():
 	till_map()
+	def carrot():
+		if can_harvest():
+			harvest()
+			plant(Entities.Carrot)
+		elif get_entity_type() != Entities.Carrot:
+			plant(Entities.Carrot)
 	while num_items(Items.Wood) > 1 and num_items(Items.Hay) > 1:
-		for i in range(get_world_size()):
-			for j in range(get_world_size()):
-				if can_harvest():
-					harvest()
-					plant(Entities.Carrot)
-				elif get_entity_type() != Entities.Carrot:
-					plant(Entities.Carrot)
-				move(North)
-			move(East)
+		for_each(carrot)
 
 def can_plant_tree():
 	x_even = get_pos_x() % 2 == 0
@@ -28,36 +30,32 @@ def can_plant_tree():
 		return True
 	return False
 
-def wood():
+def woods():
 	clear()
+	def wood():
+		if can_harvest():
+			harvest()
+		if can_plant_tree():
+			plant(Entities.Tree)
+		else:
+			plant(Entities.Bush)
 	while num_items(Items.Wood) < 1000:
-		for i in range(get_world_size()):
-			for j in range(get_world_size()):
-				if can_harvest():
-					harvest()
-				if can_plant_tree():
-					plant(Entities.Tree)
-				else:
-					plant(Entities.Bush)
-				move(North)
-			move(East)
+		for_each(wood)
 
-def hay():
+def hays():
 	clear()
+	def hay():
+		if not can_harvest():
+			do_a_flip()
+		harvest()
 	while num_items(Items.Hay) < 1000:
-		for i in range(get_world_size()):
-			for j in range(get_world_size()):
-				if not can_harvest():
-					do_a_flip()
-				harvest()
-				move(North)
-			move(East)
+		for_each(hay)
 
 
 while True:
-	if num_items(Items.Wood) > 1 and num_items(Items.Hay) > 1:
+	if num_items(Items.Wood) > 1 and num_items(Items.Hay) > 1 and num_items(Items.Carrot) < 600:
 		carrots()
 	elif num_items(Items.Hay) < 1:
-		hay()
+		hays()
 	else:
-		wood()
+		woods()
